90ee8af764dd9bff20b1f570127e9c5dfa32359e
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-05-22 17:42:52 +0200

    feat: Objects and Arrays

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,31 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20; // error - not a 'string'
-character = 'luigi';
-// age = 'yoshi'; // error - Type 'string' is not assignable to type 'number'.
-age = 40;
-// isBlackBelt = 'yes'; // error - Type 'string' is not assignable to type 'boolean'.
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+// when we declared an array with one type - the array can have only one type
+names.push('toad');
+// names.push(3); // error
+// name[0] = 3; // error
+// names = 'mario'; // err
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun'); // err
+// numbers[0] = 'shaun'; // err
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-// console.log(circ('hello')); // error
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30'; // err
+// ninja.skills = ['fighting', 'sneaking']; // don't exist obj property
+// modify an object
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,40 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
 
-// character = 20; // error - not a 'string'
-character = 'luigi';
+let names = ['luigi', 'mario', 'yoshi'];
+// when we declared an array with one type - the array can have only one type
+names.push('toad');
+// names.push(3); // error
+// name[0] = 3; // error
+// names = 'mario'; // err
 
-// age = 'yoshi'; // error - Type 'string' is not assignable to type 'number'.
-age = 40;
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shaun'); // err
+// numbers[0] = 'shaun'; // err
 
-// isBlackBelt = 'yes'; // error - Type 'string' is not assignable to type 'boolean'.
-isBlackBelt = true;
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
+
+// objects
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 }
 
-// console.log(circ('hello')); // error
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30'; // err
+// ninja.skills = ['fighting', 'sneaking']; // don't exist obj property
+
+// modify an object
+ninja = {
+    name: 'yoshi', // must to be the same type
+    belt: 'orange', // must to be the same property order
+    age: 40, // must to be here
+    // skills: ['fighting', 'sneaking'] // we can NOT add new property
+}
