662bc3c934d5df5f23cd34a9426429fe259064db
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-05-22 17:18:56 +0200

    feat: Compiling TypeScript

--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,21 @@
 /**
  * Compiling TypeScript:
- *
+ * 
  * in bash:
  * > tsc sandbox.ts sandbox.js
  * > tsc sandbox.ts // with the same name
- *
+ * 
  * to run it ones
  * > tsc sandbox.ts -w
- *
+ * 
  */
-var character = 'luigi';
+
+const character = 'luigi';
 console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs)
+
+inputs.forEach(input => {
+    console.log(input)
+})
