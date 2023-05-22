6c4ea223e872b6627f5fd295265530670d21e4f5
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-05-22 21:12:06 +0200

    feat: Better Workflow & tsconfig

---
+++ public/sandbox.js
@@ -0,0 +1,25 @@
+"use strict";
+/**
+ * --------------------------------
+ *      Better Workflow & tsconfig
+ * --------------------------------
+ *
+ * to generate tsconfig.json file:
+ * > tsc --init
+ *
+ * in tsconfig.json file:
+ *
+ * "outDir": "./public"
+ * "rootDir": "./src",
+ *
+ *  to compile files only inside the src folder add:
+ * "include": ["src"]
+ *
+ * run in bash:
+ * > tsc
+ *
+ * to compile automaticaly:
+ * > tsc -w
+ *
+ */
+console.log('test');
