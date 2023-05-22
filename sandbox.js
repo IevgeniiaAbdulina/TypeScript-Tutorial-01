a7a00d66073af5f734c576ec8f3233a3956b8346
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-05-22 20:54:07 +0200

    feat: Dynamic (any) Types

--- sandbox.js
+++ sandbox.js
@@ -1,38 +1,16 @@
-// explicit types
-var character;
 var age;
-var isLoggedIn;
-// age = 'mario'; // err
-age = 30;
-// isLoggedIn = 25; // err
-isLoggedIn = true;
-// ------------------------------------
-// arrays
-var ninjas;
-// the best practice to define at the start of the code an empty array:
-var ninjas = [];
-// then, we can do this:
-ninjas.push('mario');
-// ninjas = [10, 23]; // err
-ninjas = ['mario', 'shaun'];
-// ------------------------------------
-// union types
-var mixed = []; // () --> needs before --> []
-mixed.push(20);
-// mixed.push(false); // err
+age = 26;
+age = true;
+age = 'hello';
+age = { name: 'luigi' };
+console.log(age);
 var mixed = [];
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-// ------------------------------------
-var uid; // no need ()
-uid = '123';
-uid = 123;
-// uid = true; // err
-// ------------------------------------
-// objects
-var ninjaOne;
-ninjaOne = { name: 'mario', age: 30 };
-// ninjaOne = 'hello'; // err
-// ninjaOne = []; // err --> it could be, because of an array type of an object, but it won't work with an already declared an object before
-var ninjaTwo;
-ninjaTwo = { name: 'yoshi', age: 20, beltColor: 'black' };
-// ninjaTwo = { name: 'yoshi', age: 20, beltColor: 'black', skills: 'fluing' }; // err --> not existing argument
+console.log(mixed);
+var ninja;
+ninja = { name: 'mario', age: 25 };
+console.log(ninja);
+ninja = { name: 25, age: 'mario' };
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,52 +1,22 @@
-// explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any;
 
-// age = 'mario'; // err
-age = 30;
+age = 26;
+age = true;
+age = 'hello';
+age = { name: 'luigi' };
+console.log(age);
 
-// isLoggedIn = 25; // err
-isLoggedIn = true;
-// ------------------------------------
+let mixed: any[] = [];
 
-// arrays
-let ninjas: string[];
-// the best practice to define at the start of the code an empty array:
-let ninjas: string[] = [];
-// then, we can do this:
-ninjas.push('mario');
-
-// ninjas = [10, 23]; // err
-ninjas = ['mario', 'shaun']
-// ------------------------------------
-
-// union types
-let mixed: (string|number)[] = []; // () --> needs before --> []
-mixed.push(20);
-// mixed.push(false); // err
-
-let mixed: (string|number|boolean)[] = [];
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-// ------------------------------------
+console.log(mixed);
 
-let uid: string|number; // no need ()
-uid = '123';
-uid = 123;
-// uid = true; // err
-// ------------------------------------
+let ninja: { name: any, age: any};
 
-// objects
-let ninjaOne: object;
-ninjaOne = { name: 'mario', age: 30 };
-// ninjaOne = 'hello'; // err
-// ninjaOne = []; // err --> it could be, because of an array type of an object, but it won't work with an already declared an object before
+ninja = { name: 'mario', age: 25 };
+console.log(ninja);
 
-let ninjaTwo: {
-    name: string,
-    age: number,
-    beltColor: string
-}
-
-ninjaTwo = { name: 'yoshi', age: 20, beltColor: 'black' };
-// ninjaTwo = { name: 'yoshi', age: 20, beltColor: 'black', skills: 'fluing' }; // err --> not existing argument
+ninja = { name: 25, age: 'mario' };
+console.log(ninja)
